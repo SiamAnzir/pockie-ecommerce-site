78067aa858309b6622f4778f64fcb02a0556df61
SiamAnzir <siamanzir18@gmail.com>
2022-06-12 13:07:40 +0600

    [added] primaryContainer-component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+import PrimaryContainer from "./components/Container/PrimaryContainer";
 
 const App = () => {
   return (
     <section className="home-page">
-
+      <Navbar/>
+      <PrimaryContainer/>
     </section>
   );
 }
