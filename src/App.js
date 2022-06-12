b4128ac8d0b8e651b337facd3fbaded1e6013d0b
SiamAnzir <siamanzir18@gmail.com>
2022-06-12 18:12:44 +0600

    [Added] section-two-container-component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import React, {useEffect,useState} from "react";
 import './App.css';
+import {fetchAllProductsData} from "../src/api/index"
 import Navbar from "./components/Navbar/Navbar";
-import PrimaryContainer from "./components/Container/PrimaryContainer";
+import SectionOneContainer from "./components/Container/SectionOneContainer";
+import SectionTwoContainer from "./components/Container/SectionTwoContainer";
 
 const App = () => {
-  return (
-    <section className="home-page">
-      <Navbar/>
-      <PrimaryContainer/>
-    </section>
-  );
+    const [productData,setProductData] = useState({});
+    useEffect(
+        () => {
+            const fetchResult = async () => {
+                const result = await fetchAllProductsData();
+                setProductData(result);
+            };
+            fetchResult().then(response => response);
+        },[]
+    );
+    //console.log(productData.data);
+    return (
+        <section className="home-page">
+            <Navbar/>
+            <SectionOneContainer/>
+            <SectionTwoContainer/>
+        </section>
+    );
 }
 
 export default App;
